49ba08bf4ee69886917c75e7e48ec9cb549164b4
gatesyp <ig11@zips.uakron.edu>
2016-04-21 12:11:01 -0400

    added script to automate updating vim files

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# sets up vim and configures it 
 clear
 
 echo "Setting up vim"
@@ -8,11 +8,14 @@ sudo apt-get install vim
 sudo apt-get install ctagger
 cp -r .vim ~/
 cp .vimrc ~/
+# copy the file which will update github to reflect changes
+cp updateVimeRepo.sh ~/
 
+# install package manager
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "installing plugins"
 
 vim +PluginInstall +qall
 
-echo "all finshed"
+echo "all finished"
