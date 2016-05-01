d32c738e95334365e362cc65ca28e79c2e8b0c89
gatesyp <ig11@zips.uakron.edu>
2016-05-01 22:32:46 +0000

    update script

--- updateVimRepo.sh
+++ updateVimRepo.sh
@@ -1,8 +1,8 @@
 # script to auto update git repo when vimrc is changed, or plugins are installed
 
 # copy the updated files back to local repo
-cp .vimrc ~/GitHub/settings
-cp  .vim/plugins.vim ~/GitHub/settings/.vim
+cp ~/.vimrc ~/GitHub/settings
+cp  ~/.vim/plugins.vim ~/GitHub/settings/.vim
 
 # commit and push my changes
 cd ~/GitHub/settings
